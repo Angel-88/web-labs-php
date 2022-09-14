e3ebd502b37344bfb23772a722617d835a0981f0
Angel-88 <dasha.zagrebelna88@gmail.com>
2022-09-14 12:29:34 +0200

    Added change contact.php

--- contact.php
+++ contact.php
@@ -1,21 +1,19 @@
 <?php
 
-    if (isset($_POST['submit'])) {
-        $name = $_POST['name'];
-        $emailFrom = $_POST['email'];
-        $subject = $_POST['subject'];
-        $message = $_POST['message'];
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $emailTo = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-        $emailTo = "dasha.zagrebelna88@gmail.com";
-        $headers = "From: ".$emailFrom;
-        $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    $emailFrom = "dasha.zagrebelna88@gmail.com";
+    $headers = "From: ".$emailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-        mail($emailTo, $subject, $txt, $headers);
-
-//        if(mail($emailTo, $subject, $txt, $headers)) {
-//            header("Location: /");
-//        } else {
-//            echo "Sorry, fail to send email.";
-//        }
-    }
+        if(mail($emailTo, $subject, $txt, $headers)) {
+            header("Location: /");
+        } else {
+            echo "Sorry, fail to send email.";
+        }
+}
 ?>
