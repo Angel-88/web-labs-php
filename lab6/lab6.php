1c7db18a19dd566395ef7229ba5743b510d69595
Angel-88 <dasha.zagrebelna88@gmail.com>
2022-10-02 11:39:26 +0200

    Add lab 5 & 6

---
+++ lab6/lab6.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lab6</title>
+    <style>
+        .title {
+            text-align: center;
+        }
+        div {
+            margin: 15px 230px;
+        }
+    </style>
+</head>
+
+<body>
+<div class="wrapper">
+    <h2 class="title">Погода в містах України:</h2>
+    <img src="pogoda.php?city=kharkiv-5053">
+    <img src="pogoda.php?city=kyiv-4944">
+    <img src="pogoda.php?city=lviv-4949">
+    <img src="pogoda.php?city=cherkasy-4956">
+    <h2 class="title">Погода у світі:</h2>
+    <img src="pogoda.php?city=krakow-3212">
+    <img src="pogoda.php?city=london-744">
+    <img src="pogoda.php?city=london-744">
+    <img src="pogoda.php?city=barcelona-1948">
+</div>
+</body>
+</html>
